c647b6329d7ad314d0badb877bc0cd603596a337
HT <nguyenhoangtu99@gmail.com>
2025-04-23 23:13:06 +0700

    update log in logger

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -5,13 +5,13 @@ const logger = createLogger({
   format: format.combine(
     format.timestamp(),
     format.printf(({ timestamp, level, message }) => {
-      return `${timestamp} [${level.toUpperCase()}] ${message}`;
+      return `[${level.toUpperCase()}] ${message}`;
     })
   ),
-  transports: [
-    new transports.Console(),
-    new transports.File({ filename: 'logs/tcp-server.log' })
-  ]
+  // transports: [
+  //   new transports.Console(),
+  //   new transports.File({ filename: 'logs/tcp-server.log' })
+  // ]
 });
 
 module.exports = logger;
